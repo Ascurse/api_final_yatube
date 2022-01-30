32d086e73a7472fc10bc84acfde910a4078e65a3
Ascurse <suomi7862535@gmail.com>
2022-01-31 01:33:37 +0300

    16/33 tests passed

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -6,4 +6,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.
+        return obj.author == request.user
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from api.views import CommentViewSet, PostViewSet, GroupViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'posts/(?P<id>\d+)/comments',
+                CommentViewSet, basename='comments')
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls))
 ]
