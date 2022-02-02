0a1f2f7809d11164ac1a4b5d736dbf778b274d81
Maximilian <suomi7862535@gmail.com>
2022-02-02 16:30:31 +0300

    all tests pased 1.0.1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from rest_framework.authtoken import views
 from api.views import CommentViewSet, PostViewSet, GroupViewSet, FollowViewSet
 from rest_framework.routers import DefaultRouter
 
